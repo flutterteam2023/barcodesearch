bab1e803937eb927bc6d06bac94e079321eba87e
Kerim Altun <kerim.zigetvar44@gmail.com>
2023-04-21 04:00:27 +0300

    delete confirmed

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter TEAM hjhj'),
+      home: const MyHomePage(title: 'Flutter TEAM '),
     );
   }
 }
