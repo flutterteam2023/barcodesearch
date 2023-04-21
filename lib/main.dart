e7307ff457712a10a0b0f872e9b458422f53bd60
Kerim Altun <kerim.zigetvar44@gmail.com>
2023-04-21 03:51:05 +0300

    ilk commitimi attım

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter TEAM'),
+      home: const MyHomePage(title: 'Flutter TEAM hjhj'),
     );
   }
 }
